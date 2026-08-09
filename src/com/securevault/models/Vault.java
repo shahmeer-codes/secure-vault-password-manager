f3a502705945f621e771dc578f0a0957bd3abe91
Shahmeer Arshad <ranashahmeerarshad@gmail.com>
2026-08-09 15:52:13 +0500

    Create Vault class for credential management
    Implement Vault class to manage credentials with add, remove, and retrieve functionality.

--- src/com/securevault/models/Vault.java
+++ src/com/securevault/models/Vault.java
@@ -1,1 +1,31 @@
+package com.securevault.models;
 
+import java.util.*;
+
+public class Vault {
+    private final Map<String, Credential> credentials;
+
+    public Vault() {
+        this.credentials = new LinkedHashMap<>();
+    }
+
+    public void addCredential(Credential cred) {
+        credentials.put(cred.getService().toLowerCase(), cred);
+    }
+
+    public boolean removeCredential(String service) {
+        return credentials.remove(service.toLowerCase()) != null;
+    }
+
+    public Credential getCredential(String service) {
+        return credentials.get(service.toLowerCase());
+    }
+
+    public List<Credential> getAllCredentials() {
+        return new ArrayList<>(credentials.values());
+    }
+
+    public int getCount() {
+        return credentials.size();
+    }
+}
