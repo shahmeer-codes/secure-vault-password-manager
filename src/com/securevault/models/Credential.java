2be1784f8b50e4f778d28ef6cd3039cf90e025ff
Shahmeer Arshad <ranashahmeerarshad@gmail.com>
2026-08-09 15:51:37 +0500

    Add Credential class with service, username, and password

--- src/com/securevault/models/Credential.java
+++ src/com/securevault/models/Credential.java
@@ -1,1 +1,24 @@
 
+package com.securevault.models;
+
+public class Credential {
+    private final String service;
+    private final String username;
+    private final String password;
+
+    public Credential(String service, String username, String password) {
+        this.service = service;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getService() { return service; }
+    public String getUsername() { return username; }
+    public String getPassword() { return password; }
+
+    @Override
+    public String toString() {
+        return String.format("🌐 Service: %-20s | 👤 Username: %-15s | 🔑 Password: %s", 
+                             service, username, password);
+    }
+}
