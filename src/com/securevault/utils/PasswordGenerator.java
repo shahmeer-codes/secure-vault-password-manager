6b32de3dc44611ec43c3435465107098fe182243
Shahmeer Arshad <ranashahmeerarshad@gmail.com>
2026-08-09 16:02:09 +0500

    Add PasswordGenerator class for secure password creation
    Implemented a secure password generator that creates random passwords with a minimum length of 8 characters.

--- src/com/securevault/utils/PasswordGenerator.java
+++ src/com/securevault/utils/PasswordGenerator.java
@@ -1,1 +1,21 @@
+package com.securevault.utils;
 
+import java.security.SecureRandom;
+
+public class PasswordGenerator {
+    private static final String CHAR_LOWER = "abcdefghijklmnopqrstuvwxyz";
+    private static final String CHAR_UPPER = CHAR_LOWER.toUpperCase();
+    private static final String NUMBER = "0123456789";
+    private static final String OTHER_CHAR = "!@#$%&*()_+-=[]{}";
+    private static final String ALL_CHARS = CHAR_LOWER + CHAR_UPPER + NUMBER + OTHER_CHAR;
+    private static final SecureRandom random = new SecureRandom();
+
+    public static String generate(int length) {
+        if (length < 8) length = 8; // Minimum length
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            sb.append(ALL_CHARS.charAt(random.nextInt(ALL_CHARS.length())));
+        }
+        return sb.toString();
+    }
+}
